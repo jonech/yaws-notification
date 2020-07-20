e8cbd531201d6f21e021b0696706ba458b8bf5b9
Jone <jone@live.hk>
2020-07-20 21:03:17 +1000

    fix reschedule typo

--- src/scheduler.js
+++ src/scheduler.js
@@ -5,44 +5,50 @@ const api = require('./api');
 const constant = require('./constant');
 const fcm = require('./fcm');
 
-async function runCetusCycle(firstRun = false) {
+async function runCetusCycle(skipFCM = false) {
   let now = moment();
   console.log('Cetus Cycle Run Start: ' + now.format('LLL'));
   console.log('Fetching Cetus Cycle...');
   let cetusCycle = await api.getCetusCycle();
   console.log('Fetch Cetus Cycle complete');
   if (cetusCycle) {
-    await processCetusCycle(cetusCycle, now, firstRun);
-  }
-}
-
-async function processCetusCycle(cetusCycle, currentTime, firstRun = false) {
-  if (cetusCycle.expiry) {
-    let expiry = moment(cetusCycle.expiry);
-    console.log('Cetus Cycle Expiry: ' + expiry.format('LLL'));
-
-    let timeLeft = expiry.diff(currentTime);
-    if (timeLeft <= 0) {
-      let reschedule = currentTime.add('15', s); // rerun after 15s
-      console.log('Re-schedule Cetus Cycle on: ' + reschedule.format('LLL'));
-      schedule.scheduleJob(reschedule.toDate(), runCetusCycle);
-      return;
-    }
-
-    if (!firstRun) {
-      fcm.send({
+    let nextDate = processStat({
+      current: now,
+      expiry: cetusCycle.expiry ? moment(cetusCycle.expiry) : moment(new Date()),
+      skipFCM: skipFCM,
+      notification: {
         title: `Cetus ${cetusCycle.state}`,
         body: cetusCycle.shortString,
         platform: constant.WFPlatform.PC,
         stat: constant.WFStatType.CETUS_CYCLE
-      }).catch(err => console.error(err));
-    }
+      }
+    });
 
-    console.log('Schedule Cetus Cycle on: ' + expiry.format('LLL'));
-    schedule.scheduleJob(expiry.toDate(), runCetusCycle);
+    schedule.scheduleJob(nextDate, runCetusCycle);
   }
 }
 
+
+function processStat(options) {
+  let current = (options.current) ? options.current : moment();
+  let expiry = (options.expiry) ? options.expiry : moment();
+
+  console.log('Cetus Cycle Expiry: ' + expiry.format('LLL'));
+
+  let timeLeft = expiry.diff(current);
+  if (timeLeft <= 0) {
+    let reschedule = current.add(15, 's'); // rerun after 15s
+    console.log('Re-schedule Cetus Cycle on: ' + reschedule.format('LLL'));
+    return reschedule.toDate();
+  }
+
+  if (!options.skipFCM) {
+    fcm.send(options.notification).catch(err => console.error(err));
+  }
+
+  console.log('Schedule Cetus Cycle on: ' + expiry.format('LLL'));
+  return expiry.toDate();
+}
 
 module.exports = {
   runCetusCycle
